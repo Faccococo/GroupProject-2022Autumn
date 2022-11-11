1df45fd807567da9a3f6d241a5edf3f43bcafafe
黄梓通 <11851008+faccoco@user.noreply.gitee.com>
2022-11-11 17:16:26 +0800

    commit

--- Final_Code/pyrender/test.py
+++
@@ -1,7 +0,0 @@
-import trimesh
-import pyrender
-Baymax_trimesh = trimesh.load('../models/BaymaxWhiteOBJ/Bigmax_White_OBJ.obj')
-scene = pyrender.Scene.from_trimesh_scene(Baymax_trimesh)
-dl = pyrender.DirectionalLight(color=[1.0, 1.0, 1.0], intensity=2.0)
-scene.add(dl)
-pyrender.Viewer(scene, use_raymond_lighting=True)
